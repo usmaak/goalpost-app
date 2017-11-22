4a4618397949fc3ec84db2b68e4b14bd6a7e9c39
Scott Kilbourn <scottkilbourn@Scotts-MacBook-Pro.local>
2017-11-21 23:15:12 -0700

    Added GoalVC

---
+++ goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Scott Kilbourn on 11/21/17.
+//  Copyright © 2017 Scott Kilbourn. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addGoalButtonWasPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
