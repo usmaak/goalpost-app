1df6ba26f4888d59df6e977f84cc4a9f81872a2e
Scott Kilbourn <scottkilbourn@Scotts-MacBook-Pro.local>
2017-11-22 19:50:35 -0700

    Added GoalVC Cell

---
+++ goalpost-app/Views/GoalCell.swift
@@ -0,0 +1,21 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Scott Kilbourn on 11/21/17.
+//  Copyright © 2017 Scott Kilbourn. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    @IBOutlet weak var goalDescriptionLabel: UILabel!
+    @IBOutlet weak var goalTypeLabel: UILabel!
+    @IBOutlet weak var goalProgressLabel: UILabel!
+ 
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLabel.text = description
+        self.goalTypeLabel.text = type
+        self.goalProgressLabel.text = String(describing: goalProgressAmount)
+    }
+}
