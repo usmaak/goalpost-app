3e336b8a1fce143030afaf327b206b0e7174ecdb
Scott Kilbourn <scottkilbourn@Scotts-MacBook-Pro.local>
2017-11-23 22:21:57 -0700

    Added deleting of core data objects.

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -26,6 +26,25 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .none
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "DELETE") { (rowAction, indexPath) in
+            self.removeGoal(atIndexPath: indexPath)
+            self.fetchCoreDataObjects()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        
+        deleteAction.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        return [deleteAction]
+    }
 }
 
 extension GoalsVC {
@@ -40,6 +59,20 @@ extension GoalsVC {
         }
         catch {
             debugPrint("Could not fetch: \(error.localizedDescription)")
+        }
+    }
+    
+    func removeGoal(atIndexPath indexPath: IndexPath) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else {return}
+
+        managedContext.delete(goals[indexPath.row])
+        
+        do {
+            try managedContext.save()
+            print("Successfully removed goal.")
+        }
+        catch {
+            debugPrint("Could not delete: \(error.localizedDescription)")
         }
     }
 }
@@ -61,6 +94,12 @@ class GoalsVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+
+        fetchCoreDataObjects()
+        tableView.reloadData()
+    }
+    
+    func fetchCoreDataObjects() {
         self.fetch { (complete) in
             if goals.count >= 1 {
                 tableView.isHidden = false
@@ -69,8 +108,7 @@ class GoalsVC: UIViewController {
                 tableView.isHidden = true
             }
         }
-        
-        tableView.reloadData()
+
     }
     
     @IBAction func addGoalButtonWasPressed(_ sender: UIButton) {
